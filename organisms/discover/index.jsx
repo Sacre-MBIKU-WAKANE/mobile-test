e292bbfce9c0ca0ed4088c0829a11375fced63ba
Sacre-MBIKU <sajombiku@gmail.com>
2023-02-27 14:34:26 +0100

    feat: finish

--- organisms/discover/index.jsx
+++ organisms/discover/index.jsx
@@ -1,4 +1,5 @@
 import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { Icon } from "@rneui/themed";
 
 const width =
   Dimensions.get("screen").width + Dimensions.get("screen").width / 10;
@@ -7,7 +8,44 @@ import { Button } from "@rneui/themed";
 const Discover = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hey</Text>
+      <View style={styles.discoverTextContainer}>
+        <Text style={styles.discoverTitle}>Discover Models or Be</Text>
+        <View
+          style={{
+            paddingHorizontal: 1,
+            paddingVertical: 5,
+            backgroundColor: "#24B6AD",
+            width: 150,
+            marginBottom: 10,
+            marginTop: 5,
+          }}
+        >
+          <Text style={styles.discoverSecondTitle}>Discovered</Text>
+        </View>
+        <Text style={styles.discoverSecondDescription}>
+          A Platform that provides many kinds of the best and most trusted
+          fashion
+        </Text>
+      </View>
+      <View style={styles.discoverTextContainerSlider}>
+        <Icon
+          reverse
+          name='arrow-forward-outline'
+          type='ionicon'
+          color='#ffba02'
+        />
+        <View style={{ flexDirection: "row" }}>
+          <Button buttonStyle={{ borderRadius: 100 }} color='#bbbbbf' />
+          <Button
+            buttonStyle={{ borderRadius: 100, marginLeft: 10 }}
+            color='#bbbbbf'
+          />
+          <Button
+            buttonStyle={{ borderRadius: 100, marginLeft: 10, width: 50 }}
+            color='#ffba02'
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -18,12 +56,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     position: "relative",
-    justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#1a202c",
-    transform: [{ translateX: -20 }, { translateY: 50 }, { rotateZ: "-10deg" }],
+    transform: [
+      { translateX: -20 },
+      { translateY: 100 },
+      { rotateZ: "-10deg" },
+    ],
     width,
     borderRadius: 30,
+    paddingVertical: 35,
+  },
+  discoverTextContainer: {
+    transform: [{ rotateZ: "10deg" }],
+    paddingHorizontal: 50,
+    alignItems: "center",
+  },
+  discoverTextContainerSlider: {
+    transform: [{ translateX: -20 }, { rotateZ: "10deg" }],
+    alignItems: "center",
+    marginTop: 15,
   },
   avatar: {
     borderColor: "#fff",
@@ -35,6 +87,24 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontWeight: "600",
     fontSize: 15,
+  },
+  discoverTitle: {
+    color: "#ffba02",
+    fontWeight: "800",
+    fontSize: 20,
+  },
+  discoverSecondTitle: {
+    color: "#fff",
+    fontWeight: "800",
+    fontSize: 20,
+    textAlign: "center",
+    marginBottom: 5,
+  },
+  discoverSecondDescription: {
+    color: "#fff",
+    fontWeight: "800",
+    fontSize: 20,
+    textAlign: "center",
   },
   descriptionContainer: {
     flexDirection: "row",
